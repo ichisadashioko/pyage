d560741d897d1a92ecbd384020f60dbba87a9400
shioko <ichisadashioko@gmail.com>
2022-05-24 18:45:48 +0000

    :construction: stash progress convert AGF to PNG

--- convert_agf_to_png.py
+++ convert_agf_to_png.py
@@ -1,13 +1,48 @@
 import os
 import io
 import struct
+import argparse
+import stat
+import traceback
+import pickle
+import time
 
 from tqdm import tqdm
 
 import numpy as np
 import cv2
 
+import tqdm
+
 import shared
+
+
+def convert_rgb_to_opencv_format(
+    rgb_image: np.ndarray,
+):
+    image_shape = rgb_image.shape
+    if len(image_shape) == 2:
+        # grayscale image
+        return rgb_image
+    elif len(image_shape) == 3:
+        if image_shape[2] == 2:
+            # grayscale with alpha
+            grayscale_image = rgb_image[:, :, 0]
+            alpha_image = rgb_image[:, :, 1]
+            bgra_image = np.zeros((image_shape[0], image_shape[1], 4), dtype=np.uint8)
+            bgra_image[:, :, 0] = grayscale_image
+            bgra_image[:, :, 1] = grayscale_image
+            bgra_image[:, :, 2] = grayscale_image
+            bgra_image[:, :, 3] = alpha_image
+            return bgra_image
+        elif image_shape[2] == 3:
+            # RGB image
+            return cv2.cvtColor(rgb_image, cv2.COLOR_RGB2BGR)
+        elif image_shape[2] == 4:
+            # RGBA image
+            return cv2.cvtColor(rgb_image, cv2.COLOR_RGBA2BGRA)
+        else:
+            raise Exception(f'Unsupported image shape {image_shape}')
 
 
 def convert_agf_data_to_numpy_array(
@@ -137,8 +172,145 @@ def convert_agf_data_to_numpy_array(
             return bgr_image
 
 
+def find_agf_files(inpath: str, log_list: list):
+    file_stat = os.stat(inpath)
+    if stat.S_ISREG(file_stat.st_mode):
+        extension = os.path.splitext(inpath)[1]
+        extension = extension.lower()
+        if extension == '.agf':
+            log_list.append(inpath)
+            return
+    elif stat.S_ISDIR(file_stat.st_mode):
+        child_file_list = os.listdir(inpath)
+        for child_file in child_file_list:
+            child_fpath = os.path.join(inpath, child_file)
+            find_agf_files(child_fpath, log_list)
+
+
+def create_converting_task_list(
+    input_filepath_list: list,
+    input_dir: str,
+    output_dir: str,
+):
+    task_list = []
+
+    if output_dir == 'sameasinput':
+        for input_filepath in input_filepath_list:
+            parent_dir, filename = os.path.split(input_filepath)
+            basename, ext = os.path.splitext(filename)
+            if ext.lower() == '.png':
+                raise Exception(f'{input_filepath} has already been converted to PNG!')
+
+            output_filename = basename + '.png'
+
+            parent_dir = os.path.dirname(input_filepath)
+            output_filepath = os.path.join(parent_dir, output_filename)
+
+            task_list.append({
+                'input_filepath': input_filepath,
+                'output_filepath': output_filepath,
+            })
+    else:
+        for input_filepath in input_filepath_list:
+            rel_path = os.path.relpath(input_filepath, input_dir)
+            rel_parent, filename = os.path.split(rel_path)
+            base_filename, ext = os.path.splitext(filename)
+            if ext.lower() == '.png':
+                raise Exception(f'{input_filepath} has already been converted to PNG!')
+
+            output_filename = base_filename + '.png'
+            output_parent_dir = os.path.join(output_dir, rel_parent)
+            output_filepath = os.path.join(output_parent_dir, output_filename)
+
+            task_list.append({
+                'input_filepath': input_filepath,
+                'output_filepath': output_filepath,
+            })
+
+    return task_list
+
+
 def main():
-    pass
+    parser = argparse.ArgumentParser(description='Convert AGF to PNG')
+    parser.add_argument('inpath', help='input file path to search for AGF files')
+    parser.add_argument('outpath', nargs='?', default='sameasinput', help='path to the output directory')
+    parser.add_argument('--force', action='store_true', help='overwrite existing files')
+    parser.add_argument('-r', '--run', action='store_true', help='actually destroying your files')
+    parser.add_argument('--clean', action='store_true', help='remove PNG files')
+
+    args = parser.parse_args()
+    print('args', args)
+
+    inpath = args.inpath
+    outpath = args.outpath
+    force = args.force
+    run = args.run
+    clean = args.clean
+
+    if not os.path.exists(inpath):
+        raise Exception(f'path {inpath} does not exist')
+
+    agf_filepath_list = []
+    find_agf_files(inpath, agf_filepath_list)
+    print('len(agf_filepath_list)', len(agf_filepath_list))
+
+    task_list = create_converting_task_list(agf_filepath_list, inpath, outpath)
+
+    error_log = []
+
+    pbar = tqdm.tqdm(task_list)
+    for task_info in pbar:
+        if os.path.exists('stop'):
+            break
+
+        input_filepath = task_info['input_filepath']
+        output_filepath = task_info['output_filepath']
+        pbar.set_description(input_filepath)
+
+        if run and clean:
+            if os.path.exists(output_filepath):
+                os.remove(output_filepath)
+            continue
+
+        if not force and os.path.exists(output_filepath):
+            continue
+
+        if not run:
+            continue
+
+        agf_content_bs = open(input_filepath, 'rb').read()
+
+        try:
+            # TODO handle BMP format with minimal processing to reduce execution time
+            rgb_image = convert_agf_data_to_numpy_array(
+                agf_content_bs=agf_content_bs,
+                force_rgb=True,
+            )
+
+            cv2_image = convert_rgb_to_opencv_format(rgb_image)
+            parent_dir, filename = os.path.split(output_filepath)
+            if not os.path.exists(parent_dir):
+                os.makedirs(parent_dir)
+            cv2.imwrite(output_filepath, cv2_image, [cv2.IMWRITE_PNG_COMPRESSION, 9])
+        except Exception as ex:
+            stack_trace = traceback.format_exc()
+            print(ex)
+            print(stack_trace)
+            error_log.append({
+                'input_filepath': input_filepath,
+                'output_filepath': output_filepath,
+                'exception': ex,
+                'stack_trace': stack_trace,
+                'len(agf_content_bs)': len(agf_content_bs),
+            })
+
+    print('len(error_log)', len(error_log))
+    if len(error_log) > 0:
+        error_log_filepath = f'error_log-{time.time_ns()}.pickle'
+        print('error_log_filepath', error_log_filepath)
+
+        with open(error_log_filepath, 'wb') as outfile:
+            pickle.dump(error_log, outfile)
 
 
 if __name__ == '__main__':
